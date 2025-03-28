3857369f5bc595355ae16b31b38ec87ca2e7d956
Alexander Yoshida <alexander.yoshida@gmail.com>
2025-03-28 16:35:31 -0400

    add scheduler entry/exit tracepoints and update readme

--- lttng_trace.sh
+++ lttng_trace.sh
@@ -3,6 +3,7 @@ sudo lttng destroy sched_trace
 sudo lttng create sched_trace --output lttng_trace
 sudo lttng enable-event -u 'task_proc:*'
 sudo lttng enable-event -k 'sched*'
+sudo lttng enable-event -k 'x86_irq_vectors_reschedule*'
 sudo lttng start
 sudo ./simulate_tasks taskset.txt 5 > temp
 sudo lttng stop
