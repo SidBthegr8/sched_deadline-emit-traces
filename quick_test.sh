6cfc2ab298435c4c305e6d71828543051cd709c7
Alexander Yoshida <alexander.yoshida@gmail.com>
2025-04-20 21:29:19 -0400

    clear dirs at start of quick test

--- quick_test.sh
+++ quick_test.sh
@@ -1,4 +1,19 @@
-sh build.sh && \
-sudo sh lttng_trace.sh dl && \
-sudo chown -R alexy lttng_trace && \
-python3 ../sched_deadline-trace-parser/parse.py -rv lttng_trace
+# setup
+user=$(whoami)
+sh build.sh || exit
+[ -d lttng_trace_dl ] && rm -rf lttng_trace_dl
+[ -d lttng_trace_scx ] && rm -rf lttng_trace_scx
+[ -d lttng_trace_dl ] && rm -rf parse_output_dl
+[ -d lttng_trace_scx ] && rm -rf parse_output_scx
+
+# trace
+sudo sh lttng_trace.sh dl
+sudo chown -R "$user" lttng_trace
+mv lttng_trace lttng_trace_dl
+sudo sh lttng_trace.sh scx
+sudo chown -R "$user" lttng_trace
+mv lttng_trace lttng_trace_scx
+
+# parse
+python3 ../sched_deadline-trace-parser/parse.py -o parse_output_dl -rv lttng_trace_dl || exit
+python3 ../sched_deadline-trace-parser/parse.py -o parse_output_scx -rv lttng_trace_scx || exit
