64d57159c4d56fcf248287a205a4ed86d7c5c8e1
Alexander Yoshida <alexander.yoshida@gmail.com>
2025-04-22 18:23:15 -0400

    remove redundant code

--- quick_test.sh
+++ quick_test.sh
@@ -1,18 +1,6 @@
 user=$(whoami)
 sh build.sh || exit
 
-[ -d lttng_trace_scx_edf ] && rm -rf lttng_trace_scx_edf
-[ -d parse_output_scx_edf ] && rm -rf parse_output_scx_edf
-
-[ -d lttng_trace_dl ] && rm -rf lttng_trace_dl
-[ -d parse_output_dl ] && rm -rf parse_output_dl
-
-[ -d lttng_trace_scx_simple ] && rm -rf lttng_trace_scx_simple
-[ -d parse_output_scx_simple ] && rm -rf parse_output_scx_simple
-
-[ -d lttng_trace_fifo ] && rm -rf lttng_trace_fifo
-[ -d parse_output_fifo ] && rm -rf parse_output_fifo
-
 sched_test() {
   SCHED=$1
 
@@ -26,7 +14,7 @@ sched_test() {
   mv lttng_trace lttng_trace_$SCHED
 
   # parse
-  python3 ../sched_deadline-trace-parser/parse.py -o parse_output_$SCHED -rv lttng_trace_$SCHED || exit 1
+  python3 ../sched_deadline-trace-parser/parse.py -o parse_output_$SCHED -rv lttng_trace_$SCHED
 }
 
 sched_test scx_edf
